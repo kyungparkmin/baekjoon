e1e11e5f838b6fe35ed51f33bf9bcb1fe7cb7ccf
박경민 <87232158+kyungparkmin@users.noreply.github.com>
2022-03-02 07:48:20 +0900

    Add files via upload

---
+++ 220301/10824 네 수.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	 int a, b, c, d;
+	cin>>a>>b>>c>>d;
+	int sum1 = a+b;
+	int sum2 = c+d;
+	cout<<sum1<<sum2<<endl;
+}
---
+++ 220301/2935 소음.py
@@ -0,0 +1,8 @@
+a = int(input())
+str = str(input())
+b = int(input())
+
+if str == '*':
+  print(a*b)
+else:
+  print(a+b)
