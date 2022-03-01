be58c1a1052d0f2271c37006edfad054c5793c10
박경민 <87232158+kyungparkmin@users.noreply.github.com>
2022-03-01 13:45:54 +0900

    Update 1357 뒤집힌 덧셈.py

--- 220301/1357 뒤집힌 덧셈.py
+++ 220301/1357 뒤집힌 덧셈.py
@@ -2,9 +2,6 @@ a, b = input().split()
 a = int(a)
 b = int(b)
 
-""" reversed_str1 = "".join(reversed(a))
-reversed_str2 = "".join(reversed(b))
-sum = reversed_str1 + reversed_str2 """
 sum = int(str(a)[::-1]) + int(str(b)[::-1])
 sum1 = int(str(sum)[::-1])
 print(int(sum1))
